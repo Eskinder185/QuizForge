eab41346f2ce4e50c4623078db2e038f3e486b0f
Eskinder <eskewabe185@gmail.com>
2025-09-11 00:06:34 -0400

    deploy: 2025-09-11 00:06:32

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: mode === "production" ? "/factforge/" : "/",
+}));
